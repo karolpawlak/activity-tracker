7a3f15d1bb602100d3023a3a94b7359a1e5814dc
karolpawlak <parol.kawlak@gmail.com>
2023-03-17 14:02:43 +0000

    Functioning GET requests

--- backend/activity.go
+++ backend/activity.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+)
+
 type Activity struct {
 	ID             int     `json:"id"`
 	userName       string  `json:"user_name"`
@@ -9,6 +14,32 @@ type Activity struct {
 
 }
 
-func (a Activity) calculatePace() float32 {
+func (a *Activity) calculatePace() float32 {
 	return (float32(a.activityLength) / a.distance) / 60 // return result in minutes
+}
+
+// DB QUERIES
+
+func (a *Activity) getSingleActivity(db *sql.DB) error {
+	return db.QueryRow("SELECT * FROM activities WHERE id = ?", a.ID).Scan(&a.ID, &a.userName, &a.activityType, &a.activityLength, &a.distance)
+}
+
+func getAllActivities(db *sql.DB) ([]Activity, error) {
+	rows, err := db.Query("SELECT * FROM activities")
+	checkError(err)
+	defer rows.Close()
+
+	activities := []Activity{}
+
+	// for each row, scan the result into an activity composite object
+	for rows.Next() {
+		var activity Activity
+		err = rows.Scan(&activity.ID, &activity.userName, &activity.activityType, &activity.activityLength, &activity.distance)
+		checkError(err)
+
+		fmt.Println("ID: ", activity.ID, "Activity: ", activity.activityType, " By: ", activity.userName, " Distance: ", activity.distance)
+		activities = append(activities, activity)
+	}
+
+	return activities, err
 }
--- backend/main.go
+++ backend/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
-
 	"github.com/gorilla/mux"
 )
 
@@ -23,59 +24,95 @@ type AppConfig struct {
 	Router   *mux.Router
 }
 
-func (a *AppConfig) Run() {
-	fmt.Println("Server started and listening on port ", a.Port)
-	log.Fatal(http.ListenAndServe(a.Port, a.Router))
+func (app *AppConfig) Run() {
+	fmt.Println("Server started and listening on port ", app.Port)
+	log.Fatal(http.ListenAndServe(app.Port, app.Router))
 }
 
-func (a *AppConfig) Init() {
+func (app *AppConfig) Init() {
 	// open database connection
 	db, err := sql.Open("mysql", "root:changeme@tcp(127.0.0.1:3306)/activitytracker_db")
-	a.Database = db
+	app.Database = db
 
 	// handle the error if one occurs when opening the connection
 	checkError(err)
 
 	// create a router and initialize routes
-	a.Router = mux.NewRouter()
-	a.InitializeRoutes()
-
-	createQuery, err := a.Database.Query("INSERT INTO activities VALUES (2, 'Karol Pawlak', 'Running', 9000, 21.1)")
-	checkError(err)
-
-	defer createQuery.Close()
-
-	// defer the close till after the main function has finished executing
-	// defer db.Close()
+	app.Router = mux.NewRouter()
+	app.InitializeRoutes()
 }
 
-func (a *AppConfig) InitializeRoutes() {
+func (app *AppConfig) InitializeRoutes() {
 	fmt.Println("Routes initialized")
-	a.Router.HandleFunc("/", getRequest).Methods("GET")
-	a.Router.HandleFunc("/", postRequest).Methods("POST")
-	a.Router.HandleFunc("/", putRequest).Methods("PUT")
-	a.Router.HandleFunc("/", deleteRequest).Methods("DELETE")
-	http.Handle("/", a.Router)
+	app.Router.HandleFunc("/single/{id}", app.getRequestSingle).Methods("GET")
+	app.Router.HandleFunc("/all", app.getRequestAll).Methods("GET")
+	app.Router.HandleFunc("/", postRequest).Methods("POST")
+	app.Router.HandleFunc("/", putRequest).Methods("PUT")
+	app.Router.HandleFunc("/", deleteRequest).Methods("DELETE")
+	http.Handle("/", app.Router)
 }
 
 // MAIN
 
 func main() {
-	a := AppConfig{}
-	a.Port = port
-	a.Init()
-	a.Run()
+	app := AppConfig{}
+	app.Port = port
+	app.Init()
+	app.Run()
+
+	// defer the close till after the main function has finished executing
+	defer app.Database.Close()
 }
 
 // REQUESTS
 
-func getRequest(w http.ResponseWriter, r *http.Request) {
+func (app *AppConfig) getRequestSingle(w http.ResponseWriter, r *http.Request) {
 	t := time.Now()
 	fmt.Println("\nGET request received on ", t.Format(time.RFC3339), w)
+
+	// get variable from the request and populate in activity object
+	vars := mux.Vars(r)
+	id, err := strconv.ParseInt(vars["id"], 10, 64)
+	checkError(err)
+
+	var activity Activity
+	activity.ID = int(id)
+
+	// execute query and respond to the client
+	err = activity.getSingleActivity(app.Database)
+	if err != nil {
+		fmt.Printf("getRequest error: %s\n", err.Error())
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+
+		return
+	}
+
+	fmt.Println("ID: ", activity.ID, "Activity: ", activity.activityType, " By: ", activity.userName, " Distance: ", activity.distance)
+	respondWithJSON(w, http.StatusOK, activity)
+
+}
+
+func (app *AppConfig) getRequestAll(w http.ResponseWriter, r *http.Request) {
+	t := time.Now()
+	fmt.Println("\nGET request received on ", t.Format(time.RFC3339), w)
+
+	// execute query and respond to the client
+	activities, err := getAllActivities(app.Database)
+	if err != nil {
+		fmt.Printf("getRequest error: %s\n", err.Error())
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, activities)
 }
 
 func postRequest(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("\nPOST request received", w)
+
+	activity := Activity{1, "Karol", "Running", 2400, 8.0}
+	fmt.Println(activity.calculatePace())
 }
 
 func putRequest(w http.ResponseWriter, r *http.Request) {
@@ -86,32 +123,38 @@ func deleteRequest(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("\nDELETE request received", w)
 }
 
-// ERROR CHECKING
-
-func checkError(err error) {
-	if err != nil {
-		log.Fatal(err.Error())
-		//panic(err.Error())
-		//log.Fatal(err)
-	}
-}
-
-// REQUESTS
-
 // func basicRequest(w http.ResponseWriter, r *http.Request) {
 // 	fmt.Fprint(w, appStatus)
 // 	a := Activity{"Karol", "Running", 2400, 8.0}
 // 	fmt.Println(a.calculatePace())
 // }
 
-// func getRows() {
-// 	rows, err := db.Query("SELECT * FROM activities")
-// 	checkError(err)
-// 	defer rows.Close()
+// createQuery, err := app.Database.Query("INSERT INTO activities VALUES (2, 'Karol Pawlak', 'Running', 9000, 21.1)")
+// checkError(err)
+// defer createQuery.Close()
 
-// 	for rows.Next() {
-// 		var a Activity
-// 		rows.Scan(&a.userName, &a.activityType, &a.activityLength, a.distance)
-// 		fmt.Println("Activity: ", a.activityType, " By: ", a.userName, " Distance: ", a.distance)
-// 	}
-// }
+// ERROR CHECKING
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err.Error())
+		//panic(err.Error()) // proper error handling instead of panic in your app
+		//log.Fatal(err)
+	}
+}
+
+// HELPER FUNCTIONS
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, map[string]string{"error": message}) // package the error and call respondWithJSON function
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	// create a response
+	response, _ := json.Marshal(payload)
+
+	// set headers and write a response
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
